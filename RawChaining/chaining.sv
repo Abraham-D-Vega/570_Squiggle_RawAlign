919e6b434a8f99f7e20618313a3a9907dd9fe261
Abraham-Daniel-Vega <abrahamvegaa@hotmail.com>
2025-11-19 21:00:26 -0500

    added more

--- RawChaining/chaining.sv
+++ RawChaining/chaining.sv
@@ -23,4 +23,50 @@ void comp_mapq(std::vector<ri_chain_t> &chains, const ri_mapopt_t *opt) {
   }
 }
 
-module comp_mapq
+`define RI_M_DTW_EVALUATE_CHAINS	0x2
+//#define RI_M_DTW_EVALUATE_CHAINS	0x2
+
+
+// COMPUTE MAPQ (variable of ri_chain_t)
+module comp_mapq #(
+    parameter NUM_CHAINS = 16
+) (
+    //INPUTS
+    input ri_chain_t input_chains [NUM_CHAINS],
+    input ri_mapopt_t opt,
+    output ri_chain_t output_chain_MAPQ [NUM_CHAINS]
+);
+    
+    ri_chain_t temp [NUM_CHAINS];
+    temp = input_chains;
+
+    if(NUM_CHAINS == 1){
+        temp[0].mapq = 60;
+    }
+    else{
+        reg [31:0] mapq;
+        reg [7:0] mapq8;
+        if(opt.flag & RI_M_DTW_EVALUATE_CHAINS){
+            //need multiplier for this part of the chaining
+        }
+        else{
+            //need multiplier for this part of the chaining
+        }
+
+        if (mapq > 60) {
+            mapq = 60;
+        }
+        if (mapq < 0) {
+            mapq = 0;
+        }
+        mapq8 = mapq[7:0];
+        temp[0].mapq = mapq8;
+    }
+
+    always_comb begin 
+        output_chain_MAPQ =  temp;
+        //OR
+        for (int i = 0; i < NUM_CHAINS; i++) begin
+            output_chain_MAPQ[i] = temp[i];
+        end
+    end
--- RawChaining/chaining.svh
+++ RawChaining/chaining.svh
@@ -37,3 +37,53 @@ typedef struct packed {
     alignment_element alignment [DEFAULT_ALIGNMENTS];
     logic [$clog2(DEFAULT_ALIGNMENTS):0] alignment_size;
 } dtw_result;
+
+
+typedef struct packed {
+    // ONT Device specific parameters
+    logic [31:0] bp_per_sec;
+    logic [31:0] sample_rate;
+    logic [31:0] chunk_size;
+
+    // Chaining parameters
+    logic [31:0] min_events;
+    logic [31:0] max_gap_length;
+    logic [31:0] max_target_gap_length;
+    logic [31:0] chaining_band_length;
+    logic [31:0] max_num_skips;
+    logic [31:0] min_num_anchors;
+    logic [31:0] num_best_chains;
+    logic [31:0]    min_chaining_score;
+
+    // Mapping parameters
+    logic [31:0] step_size;
+    logic [31:0] max_num_chunk;
+    logic [31:0] min_chain_anchor;
+    logic [31:0] min_chain_anchor_out;
+    logic [31:0] dtw_border_constraint;
+    logic [31:0] dtw_fill_method;
+    logic [31:0] dtw_band_radius_frac;
+    logic [31:0] dtw_match_bonus;
+    logic [31:0] dtw_min_score;
+
+    logic [31:0] min_bestmap_ratio;
+    logic [31:0] min_bestmap_ratio_out;
+
+    logic [31:0] min_meanmap_ratio;
+    logic [31:0] min_meanmap_ratio_out;
+
+    logic [31:0] t_threshold;
+    logic [31:0] tn_samples;
+    logic [31:0] ttest_freq;
+    logic [31:0] tmin_reads;
+
+    logic signed [63:0] flag;             // int64_t
+    logic signed [63:0] mini_batch_size;  // int64_t
+
+    // Event detector options
+    logic [31:0] window_length1;
+    logic [31:0] window_length2;
+    logic [31:0] threshold1;
+    logic [31:0] threshold2;
+    logic [31:0] peak_height;
+} ri_mapopt_t;
