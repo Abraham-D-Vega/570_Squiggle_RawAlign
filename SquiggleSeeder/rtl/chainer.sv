72b630b35138955db4e1a698de087c5479c630fa
Matt <dahlmanm@umich.edu>
2025-12-01 21:09:15 -0500

    fixing syntax

--- SquiggleSeeder/rtl/chainer.sv
+++ SquiggleSeeder/rtl/chainer.sv
@@ -1,8 +1,10 @@
 //Chain Anchors and provide input to Aligner
+
+`include "utils.svh"
 module Chainer(
     input Anchor seeds [`MAX_NUM_SEEDS-1: 0],
-    output Anchor chains [`MAX_NUM_CHAINS-1:0][`MAX_NUM_SEEDS_IN_SEG-1: 0]
-)
+    output Anchor chains [`MAX_NUM_CHAINS-1:0][`MAX_NUM_SEEDS-1: 0]
+);
 
     // -----------------------------
     // 2. Build overlapping segments in reference space
@@ -11,13 +13,8 @@ module Chainer(
     // 
     logic [31:0] seg_begin [`NUM_SEGMENTS];//Beginning index of each reference segment in seeds
     logic [31:0] seg_end [`NUM_SEGMENTS];//End index of each reference segment in seeds
-    logic [31:0] r_min; 
-    logic [31:0] r_max;
-    Chain chains_per_segment [`NUM_SEGMENTS][`MAX_CHAINS]
-    r_min = seeds[0].r;
-    r_max = seeds[`MAX_NUM_SEEDS-1].r;
-
-
+    Chain chains_per_segment [`NUM_SEGMENTS][`MAX_NUM_CHAINS];
+    
     segment_characterizer seg_char (
         seeds,
         seg_begin,
@@ -35,31 +32,41 @@ module Chainer(
         chains_per_segment
     );
 
-    //TODO: Add interface for step 4
-    Chain [`NUM_SEGMENTS*`MAX_CHAINS-1:0]  chain_in;
+
+
+    // ---------------------------
+    // 4. Find top five non overlapping chains
+    // ---------------------------
+
+    Chain [`NUM_SEGMENTS*`MAX_NUM_CHAINS-1:0]  chain_in;
     assign chain_in = chains_per_segment;
 
     logic [31:0] valid_mask [`MAX_NUM_SEEDS];
     always_comb begin
-        for (int j = 0; j < (`NUM_SEGMENTS*`MAX_CHAINS); j++) begin
+        for (int j = 0; j < (`NUM_SEGMENTS*`MAX_NUM_CHAINS); j++) begin
             valid_mask[j] = (chain_in[j].score > 32'h0) ? 32'h1 : 32'h0;
         end
     end
-    Chain [`MAX_CHAINS-1:0] final_chains;
+    Chain [`MAX_NUM_CHAINS-1:0] final_chains;
     logic [31:0] valid_out [`MAX_NUM_SEEDS];
 
-    chain_sorter(
+    chain_sorter  #(
+        .NUM_INPUT_CHAINS(`NUM_SEGMENTS*`MAX_NUM_CHAINS),
+        .NUM_OUTPUT_CHAINS(`MAX_NUM_CHAINS)
+    )
+    top_level_sorter
+    (
         .chain_in(chain_in),
         .valid(valid_mask),
         .chain_out(final_chains),
         .valid_out(valid_out)
     );
 
-    always_comb {
+    always_comb begin
         chains = '0;
-        for(int i = 0; i < `MAX_CHAINS; i++) begin
-            for(int j = 0; j < `NUM_SEEDS; j++) begin
-                for(int k = 0; k < `NUM_SEEDS; k++) begin
+        for(int i = 0; i < `MAX_NUM_CHAINS; i++) begin
+            for(int j = 0; j < `MAX_NUM_SEEDS; j++) begin
+                for(int k = 0; k < `MAX_NUM_SEEDS; k++) begin
                     if(valid_out[i]) begin
                         if(final_chains.anchors[k]) begin
                             chains[i][j] = seeds[k];
@@ -69,5 +76,6 @@ module Chainer(
                 end
             end
         end
-    }
+    end
+
 endmodule
