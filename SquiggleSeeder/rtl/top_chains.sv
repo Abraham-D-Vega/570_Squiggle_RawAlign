4f58eb0585aded5c83f8897ae55fbf262747f8ff
Matt <dahlmanm@umich.edu>
2025-12-07 21:04:21 -0500

    fix ties

--- SquiggleSeeder/rtl/top_chains.sv
+++ SquiggleSeeder/rtl/top_chains.sv
@@ -10,24 +10,25 @@ module top_chains(
     output Anchor [`MAX_NUM_CHAINS-1:0] chain_ends
 ); 
     logic [`MAX_NUM_CHAINS-1:0][31:0] top_scores;
-
+    logic[`NUM_SEGMENTS-1:0] visited;
+    logic [31:0] idx;
     always_comb begin
         top_scores = '0;
         chain_ends = '0;
         chain_starts = '0;
-        for(int i = 0; i < `NUM_SEGMENTS; i++) begin
-            if(top_scores[0] < scores_in[i]) begin
-                top_scores[0] = scores_in[i];
-                chain_starts[0] = starts_in[i];
-                chain_ends[0] = ends_in[i];
-            end
-            for(int j = 1; j < `MAX_NUM_CHAINS; j++) begin
-                if(top_scores[j] < scores_in[i] && scores_in[i] < top_scores[j-1]) begin
-                    top_scores[j] = scores_in[i];
-                    chain_starts[j] = starts_in[i];
-                    chain_ends[j] = ends_in[i];
+        visited = '0;
+        
+        for(int i = 0; i < `MAX_NUM_CHAINS; i++) begin
+            idx = '0;
+            for(int j = 0; j < `NUM_SEGMENTS; j++) begin
+                if(~visited[j] && (scores_in[j] > top_scores[i])) begin
+                    top_scores[i] = scores_in[j];
+                    chain_starts[i] = starts_in[j];
+                    chain_ends[i] = ends_in[j];
+                    idx = j;
                 end
             end
+            visited[idx] = 1'b1;
         end
     end
 endmodule
